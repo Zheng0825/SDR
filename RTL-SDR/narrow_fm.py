98a04489d46639703c01c6d9dcf28e6c88f0d90a
John McCormack <mccormack.wgsi@gmail.com>
2015-07-31 12:21:32 -0400

    Updated Bibliography

--- RTL-SDR/narrow_fm.py
+++ RTL-SDR/narrow_fm.py
@@ -2,7 +2,7 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Narrow Fm
-# Generated: Mon Jul  6 17:05:19 2015
+# Generated: Wed Jul 29 09:38:16 2015
 ##################################################
 
 from gnuradio import analog
@@ -31,11 +31,9 @@ class narrow_fm(grc_wxgui.top_block_gui):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 1400000
+        self.samp_rate = samp_rate = 2000000
         self.corr = corr = 0
-        self.Gain2 = Gain2 = 40
-        self.Gain1 = Gain1 = 40
-        self.Freq = Freq = 108e6
+        self.Freq = Freq = 107.9e6
 
         ##################################################
         # Blocks
@@ -63,52 +61,6 @@ class narrow_fm(grc_wxgui.top_block_gui):
         	proportion=1,
         )
         self.Add(_corr_sizer)
-        _Gain2_sizer = wx.BoxSizer(wx.VERTICAL)
-        self._Gain2_text_box = forms.text_box(
-        	parent=self.GetWin(),
-        	sizer=_Gain2_sizer,
-        	value=self.Gain2,
-        	callback=self.set_Gain2,
-        	label='Gain2',
-        	converter=forms.float_converter(),
-        	proportion=0,
-        )
-        self._Gain2_slider = forms.slider(
-        	parent=self.GetWin(),
-        	sizer=_Gain2_sizer,
-        	value=self.Gain2,
-        	callback=self.set_Gain2,
-        	minimum=0,
-        	maximum=50,
-        	num_steps=100,
-        	style=wx.SL_HORIZONTAL,
-        	cast=float,
-        	proportion=1,
-        )
-        self.Add(_Gain2_sizer)
-        _Gain1_sizer = wx.BoxSizer(wx.VERTICAL)
-        self._Gain1_text_box = forms.text_box(
-        	parent=self.GetWin(),
-        	sizer=_Gain1_sizer,
-        	value=self.Gain1,
-        	callback=self.set_Gain1,
-        	label='Gain1',
-        	converter=forms.float_converter(),
-        	proportion=0,
-        )
-        self._Gain1_slider = forms.slider(
-        	parent=self.GetWin(),
-        	sizer=_Gain1_sizer,
-        	value=self.Gain1,
-        	callback=self.set_Gain1,
-        	minimum=0,
-        	maximum=50,
-        	num_steps=100,
-        	style=wx.SL_HORIZONTAL,
-        	cast=float,
-        	proportion=1,
-        )
-        self.Add(_Gain1_sizer)
         _Freq_sizer = wx.BoxSizer(wx.VERTICAL)
         self._Freq_text_box = forms.text_box(
         	parent=self.GetWin(),
@@ -126,7 +78,7 @@ class narrow_fm(grc_wxgui.top_block_gui):
         	callback=self.set_Freq,
         	minimum=80e6,
         	maximum=115e6,
-        	num_steps=100,
+        	num_steps=1000,
         	style=wx.SL_HORIZONTAL,
         	cast=float,
         	proportion=1,
@@ -150,7 +102,7 @@ class narrow_fm(grc_wxgui.top_block_gui):
         self.Add(self.wxgui_fftsink2_0.win)
         self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
                 interpolation=48,
-                decimation=240,
+                decimation=500,
                 taps=None,
                 fractional_bw=None,
         )
@@ -158,20 +110,20 @@ class narrow_fm(grc_wxgui.top_block_gui):
         self.osmosdr_source_0.set_sample_rate(samp_rate)
         self.osmosdr_source_0.set_center_freq(Freq, 0)
         self.osmosdr_source_0.set_freq_corr(corr, 0)
-        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
-        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
-        self.osmosdr_source_0.set_gain_mode(0, 0)
-        self.osmosdr_source_0.set_gain(Gain1, 0)
-        self.osmosdr_source_0.set_if_gain(Gain2, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(2, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(2, 0)
+        self.osmosdr_source_0.set_gain_mode(1, 0)
+        self.osmosdr_source_0.set_gain(50, 0)
+        self.osmosdr_source_0.set_if_gain(50, 0)
         self.osmosdr_source_0.set_bb_gain(20, 0)
         self.osmosdr_source_0.set_antenna("", 0)
         self.osmosdr_source_0.set_bandwidth(0, 0)
           
-        self.low_pass_filter_0 = filter.fir_filter_ccf(5, firdes.low_pass(
-        	1, samp_rate, 100e3, 10e3, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0 = filter.fir_filter_ccf(int(samp_rate/500e3), firdes.low_pass(
+        	1, samp_rate, 100e3, 1e6, firdes.WIN_HAMMING, 6.76))
         self.audio_sink_0 = audio.sink(48000, "", True)
         self.analog_wfm_rcv_0 = analog.wfm_rcv(
-        	quad_rate=240e3,
+        	quad_rate=500e3,
         	audio_decimation=1,
         )
 
@@ -192,8 +144,8 @@ class narrow_fm(grc_wxgui.top_block_gui):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 100e3, 1e6, firdes.WIN_HAMMING, 6.76))
         self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate / 25)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 100e3, 10e3, firdes.WIN_HAMMING, 6.76))
         self.osmosdr_source_0.set_sample_rate(self.samp_rate)
 
     def get_corr(self):
@@ -205,33 +157,15 @@ class narrow_fm(grc_wxgui.top_block_gui):
         self._corr_text_box.set_value(self.corr)
         self.osmosdr_source_0.set_freq_corr(self.corr, 0)
 
-    def get_Gain2(self):
-        return self.Gain2
-
-    def set_Gain2(self, Gain2):
-        self.Gain2 = Gain2
-        self._Gain2_slider.set_value(self.Gain2)
-        self._Gain2_text_box.set_value(self.Gain2)
-        self.osmosdr_source_0.set_if_gain(self.Gain2, 0)
-
-    def get_Gain1(self):
-        return self.Gain1
-
-    def set_Gain1(self, Gain1):
-        self.Gain1 = Gain1
-        self._Gain1_slider.set_value(self.Gain1)
-        self._Gain1_text_box.set_value(self.Gain1)
-        self.osmosdr_source_0.set_gain(self.Gain1, 0)
-
     def get_Freq(self):
         return self.Freq
 
     def set_Freq(self, Freq):
         self.Freq = Freq
-        self._Freq_slider.set_value(self.Freq)
-        self._Freq_text_box.set_value(self.Freq)
         self.wxgui_fftsink2_0.set_baseband_freq(self.Freq)
         self.osmosdr_source_0.set_center_freq(self.Freq, 0)
+        self._Freq_slider.set_value(self.Freq)
+        self._Freq_text_box.set_value(self.Freq)
 
 if __name__ == '__main__':
     import ctypes
