1da96e041db9811c475e5e4a6673c901b172a0d5
John McCormack <mccormack.wgsi@gmail.com>
2015-11-22 13:34:04 -0500

    Something happened with the txrxFM graph but it has been fixed

--- SDR/USRP_B200/WPI/Lab2_src/top_block.py
+++ SDR/USRP_B200/WPI/Lab2_src/top_block.py
@@ -2,7 +2,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Sat Nov 21 15:06:45 2015
+# Generated: Sun Nov 22 12:54:18 2015
 ##################################################
 
 if __name__ == '__main__':
@@ -15,15 +15,13 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
-from gnuradio import analog
 from gnuradio import blocks
+from gnuradio import digital
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio import uhd
-from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
-from gnuradio.wxgui import scopesink2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import time
@@ -32,56 +30,19 @@ import wx
 
 class top_block(grc_wxgui.top_block_gui):
 
-    def __init__(self, address="addr=192.168.10.2", freq=5.31e6, freq_offset=4492.1875, gain=30, samp_rate_0=1e6):
+    def __init__(self):
         grc_wxgui.top_block_gui.__init__(self, title="Top Block")
         _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
         self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
         ##################################################
-        # Parameters
-        ##################################################
-        self.address = address
-        self.freq = freq
-        self.freq_offset = freq_offset
-        self.gain = gain
-        self.samp_rate_0 = samp_rate_0
-
-        ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 100e6/256
+        self.samp_rate = samp_rate = 1000000
 
         ##################################################
         # Blocks
         ##################################################
-        self.wxgui_scopesink2_0_1 = scopesink2.scope_sink_f(
-        	self.GetWin(),
-        	title="Scope Plot (Imag)",
-        	sample_rate=samp_rate,
-        	v_scale=0,
-        	v_offset=0,
-        	t_scale=0,
-        	ac_couple=False,
-        	xy_mode=False,
-        	num_inputs=1,
-        	trig_mode=wxgui.TRIG_MODE_AUTO,
-        	y_axis_label="Counts",
-        )
-        self.Add(self.wxgui_scopesink2_0_1.win)
-        self.wxgui_scopesink2_0_0 = scopesink2.scope_sink_f(
-        	self.GetWin(),
-        	title="Scope Plot (Real)",
-        	sample_rate=samp_rate,
-        	v_scale=0,
-        	v_offset=0,
-        	t_scale=0,
-        	ac_couple=False,
-        	xy_mode=False,
-        	num_inputs=1,
-        	trig_mode=wxgui.TRIG_MODE_NORM,
-        	y_axis_label="Counts",
-        )
-        self.Add(self.wxgui_scopesink2_0_0.win)
         self.uhd_usrp_source_0 = uhd.usrp_source(
         	",".join(("", "")),
         	uhd.stream_args(
@@ -90,8 +51,8 @@ class top_block(grc_wxgui.top_block_gui):
         	),
         )
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_source_0.set_center_freq(freq+freq_offset, 0)
-        self.uhd_usrp_source_0.set_gain(gain, 0)
+        self.uhd_usrp_source_0.set_center_freq(88.1e6, 0)
+        self.uhd_usrp_source_0.set_gain(0, 0)
         self.uhd_usrp_source_0.set_antenna("RX2", 0)
         self.uhd_usrp_sink_0 = uhd.usrp_sink(
         	",".join(("", "")),
@@ -101,85 +62,53 @@ class top_block(grc_wxgui.top_block_gui):
         	),
         )
         self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_sink_0.set_center_freq(freq, 0)
-        self.uhd_usrp_sink_0.set_gain(gain, 0)
+        self.uhd_usrp_sink_0.set_center_freq(88.1e6, 0)
+        self.uhd_usrp_sink_0.set_gain(0, 0)
         self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
-        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
-        self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
-        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, 20000, 1, 0)
-        self.analog_const_source_x_0_0_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 0)
+        self.digital_dxpsk_mod_0 = digital.dbpsk_mod(
+        	samples_per_symbol=2,
+        	excess_bw=0.35,
+        	mod_code="gray",
+        	verbose=False,
+        	log=False)
+        	
+        self.digital_dxpsk_demod_0 = digital.dbpsk_demod(
+        	samples_per_symbol=2,
+        	excess_bw=0.35,
+        	freq_bw=6.28/100.0,
+        	phase_bw=6.28/100.0,
+        	timing_bw=6.28/100.0,
+        	mod_code="gray",
+        	verbose=False,
+        	log=False
+        )
+        self.blocks_vector_source_x_0 = blocks.vector_source_b([1,1], True, 1, [])
+        self.blocks_unpacked_to_packed_xx_0 = blocks.unpacked_to_packed_bb(1, gr.GR_MSB_FIRST)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, "dbpsk_out.txt", False)
+        self.blocks_file_sink_0.set_unbuffered(True)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_const_source_x_0_0_0, 0), (self.blocks_float_to_complex_0, 1))    
-        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_float_to_complex_0, 0))    
-        self.connect((self.blocks_complex_to_float_0, 0), (self.wxgui_scopesink2_0_0, 0))    
-        self.connect((self.blocks_complex_to_float_0, 1), (self.wxgui_scopesink2_0_1, 0))    
-        self.connect((self.blocks_float_to_complex_0, 0), (self.uhd_usrp_sink_0, 0))    
-        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_complex_to_float_0, 0))    
+        self.connect((self.blocks_unpacked_to_packed_xx_0, 0), (self.digital_dxpsk_mod_0, 0))    
+        self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_unpacked_to_packed_xx_0, 0))    
+        self.connect((self.digital_dxpsk_demod_0, 0), (self.blocks_file_sink_0, 0))    
+        self.connect((self.digital_dxpsk_mod_0, 0), (self.uhd_usrp_sink_0, 0))    
+        self.connect((self.uhd_usrp_source_0, 0), (self.digital_dxpsk_demod_0, 0))    
 
-
-    def get_address(self):
-        return self.address
-
-    def set_address(self, address):
-        self.address = address
-
-    def get_freq(self):
-        return self.freq
-
-    def set_freq(self, freq):
-        self.freq = freq
-        self.uhd_usrp_sink_0.set_center_freq(self.freq, 0)
-        self.uhd_usrp_source_0.set_center_freq(self.freq+self.freq_offset, 0)
-
-    def get_freq_offset(self):
-        return self.freq_offset
-
-    def set_freq_offset(self, freq_offset):
-        self.freq_offset = freq_offset
-        self.uhd_usrp_source_0.set_center_freq(self.freq+self.freq_offset, 0)
-
-    def get_gain(self):
-        return self.gain
-
-    def set_gain(self, gain):
-        self.gain = gain
-        self.uhd_usrp_sink_0.set_gain(self.gain, 0)
-        self.uhd_usrp_source_0.set_gain(self.gain, 0)
-
-    def get_samp_rate_0(self):
-        return self.samp_rate_0
-
-    def set_samp_rate_0(self, samp_rate_0):
-        self.samp_rate_0 = samp_rate_0
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
         self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
-        self.wxgui_scopesink2_0_0.set_sample_rate(self.samp_rate)
-        self.wxgui_scopesink2_0_1.set_sample_rate(self.samp_rate)
 
 
 if __name__ == '__main__':
     parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
-    parser.add_option("-a", "--address", dest="address", type="string", default="addr=192.168.10.2",
-        help="Set IP Address [default=%default]")
-    parser.add_option("-f", "--freq", dest="freq", type="eng_float", default=eng_notation.num_to_str(5.31e6),
-        help="Set Default Frequency [default=%default]")
-    parser.add_option("-o", "--freq-offset", dest="freq_offset", type="eng_float", default=eng_notation.num_to_str(4492.1875),
-        help="Set Rx Frequency Offset [default=%default]")
-    parser.add_option("-g", "--gain", dest="gain", type="eng_float", default=eng_notation.num_to_str(30),
-        help="Set Default Gain [default=%default]")
-    parser.add_option("-s", "--samp-rate-0", dest="samp_rate_0", type="eng_float", default=eng_notation.num_to_str(1e6),
-        help="Set Sample Rate [default=%default]")
     (options, args) = parser.parse_args()
-    tb = top_block(address=options.address, freq=options.freq, freq_offset=options.freq_offset, gain=options.gain, samp_rate_0=options.samp_rate_0)
+    tb = top_block()
     tb.Start(True)
     tb.Wait()
