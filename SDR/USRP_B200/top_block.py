d1d4f0a93f54424d884466f17460ac3759f69f85
John McCormack <mccormack.wgsi@gmail.com>
2015-11-22 15:05:52 -0500

    Now able to send packets over the air, but it seems certain packets are starting to drop

--- SDR/USRP_B200/top_block.py
+++ SDR/USRP_B200/top_block.py
@@ -2,7 +2,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Sun Nov 22 13:53:32 2015
+# Generated: Sun Nov 22 14:57:33 2015
 ##################################################
 
 if __name__ == '__main__':
@@ -19,6 +19,7 @@ from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
 from gnuradio import gr
+from gnuradio import uhd
 from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
@@ -26,6 +27,7 @@ from gnuradio.wxgui import scopesink2
 from grc_gnuradio import blks2 as grc_blks2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
+import time
 import wx
 
 
@@ -39,13 +41,27 @@ class top_block(grc_wxgui.top_block_gui):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32E3
+        self.samp_rate = samp_rate = 1e6
         self.code2 = code2 = '11011010110111011000110011110101100010010011110111'
         self.code1 = code1 = '010110011011101100010101011111101001001110001011010001101010001'
 
         ##################################################
         # Blocks
         ##################################################
+        self.wxgui_scopesink2_1 = scopesink2.scope_sink_c(
+        	self.GetWin(),
+        	title="Scope Plot",
+        	sample_rate=samp_rate,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label="Counts",
+        )
+        self.Add(self.wxgui_scopesink2_1.win)
         self.wxgui_scopesink2_0 = scopesink2.scope_sink_f(
         	self.GetWin(),
         	title="Scope Plot",
@@ -60,6 +76,28 @@ class top_block(grc_wxgui.top_block_gui):
         	y_axis_label="Counts",
         )
         self.Add(self.wxgui_scopesink2_0.win)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+        	",".join(("", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(88.1e6, 0)
+        self.uhd_usrp_source_0.set_normalized_gain(.5, 0)
+        self.uhd_usrp_source_0.set_antenna("RX2", 0)
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+        	",".join(("", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_sink_0.set_center_freq(88.1e6, 0)
+        self.uhd_usrp_sink_0.set_normalized_gain(.5, 0)
+        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
         self.digital_dxpsk_mod_0 = digital.dbpsk_mod(
         	samples_per_symbol=2,
         	excess_bw=0.35,
@@ -79,7 +117,6 @@ class top_block(grc_wxgui.top_block_gui):
         )
         self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bf((0,1), 1)
         self.blocks_udp_source_0 = blocks.udp_source(gr.sizeof_char*1, "127.0.0.1", 1234, 1472, True)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
         self.blocks_packed_to_unpacked_xx_1 = blocks.packed_to_unpacked_bb(1, gr.GR_MSB_FIRST)
         self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, "/home/john/IRG/SDR/USRP_B200/output.txt", True)
         self.blocks_file_sink_0.set_unbuffered(True)
@@ -88,7 +125,7 @@ class top_block(grc_wxgui.top_block_gui):
         		bits_per_symbol=1,
         		preamble="",
         		access_code=code1,
-        		pad_for_usrp=False,
+        		pad_for_usrp=True,
         	),
         	payload_length=4,
         )
@@ -105,12 +142,13 @@ class top_block(grc_wxgui.top_block_gui):
         self.connect((self.blks2_packet_decoder_0, 0), (self.blocks_file_sink_0, 0))    
         self.connect((self.blks2_packet_encoder_0_0, 0), (self.digital_dxpsk_mod_0, 0))    
         self.connect((self.blocks_packed_to_unpacked_xx_1, 0), (self.digital_chunks_to_symbols_xx_0, 0))    
-        self.connect((self.blocks_throttle_0, 0), (self.blks2_packet_encoder_0_0, 0))    
+        self.connect((self.blocks_udp_source_0, 0), (self.blks2_packet_encoder_0_0, 0))    
         self.connect((self.blocks_udp_source_0, 0), (self.blocks_packed_to_unpacked_xx_1, 0))    
-        self.connect((self.blocks_udp_source_0, 0), (self.blocks_throttle_0, 0))    
         self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.wxgui_scopesink2_0, 0))    
         self.connect((self.digital_dxpsk_demod_0, 0), (self.blks2_packet_decoder_0, 0))    
-        self.connect((self.digital_dxpsk_mod_0, 0), (self.digital_dxpsk_demod_0, 0))    
+        self.connect((self.digital_dxpsk_mod_0, 0), (self.uhd_usrp_sink_0, 0))    
+        self.connect((self.uhd_usrp_source_0, 0), (self.digital_dxpsk_demod_0, 0))    
+        self.connect((self.uhd_usrp_source_0, 0), (self.wxgui_scopesink2_1, 0))    
 
 
     def get_samp_rate(self):
@@ -118,8 +156,10 @@ class top_block(grc_wxgui.top_block_gui):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
         self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_scopesink2_1.set_sample_rate(self.samp_rate)
 
     def get_code2(self):
         return self.code2
--- Tools/UDP/sendUDP.py
+++ Tools/UDP/sendUDP.py
@@ -1,8 +1,9 @@
 import socket
+import time
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 8084
-MESSAGE = "Hello SDR World!"
+UDP_PORT = 1234
+MESSAGE = "Hello SDR World! I'm like hey whats up hello! I just called to say Hello. Hello Hello Hola"
 
 print "UDP target IP: ", UDP_IP
 print "UDP target Port: ", UDP_PORT
@@ -13,4 +14,4 @@ while(1):
 	MESSAGE_SENT = MESSAGE
 	print "Sent"
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.sendto(MESSAGE_SENT, (UDP_IP, UDP_PORT))
+        sock.sendto(MESSAGE_SENT, (UDP_IP, UDP_PORT))
