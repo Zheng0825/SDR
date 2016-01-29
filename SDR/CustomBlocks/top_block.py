53d1c361e8f029a5fa6730413f5f73bc918d4e64
John McCormack <mccormack.wgsi@gmail.com>
2016-01-29 11:43:37 -0500

    Updated the protocol seclector in order to run a test to see what is causing the program to crash

---
+++ SDR/CustomBlocks/top_block.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python2
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Fri Jan 29 11:34:18 2016
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import uhd
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.wxgui import forms
+from grc_gnuradio import blks2 as grc_blks2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import time
+import wx
+
+
+class top_block(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 250e3
+        self.freq = freq = 88.5e6
+        self.enable = enable = 0
+        self.audio_rate = audio_rate = 44100
+        self.audio_interp = audio_interp = 4
+
+        ##################################################
+        # Blocks
+        ##################################################
+        _freq_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._freq_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_freq_sizer,
+        	value=self.freq,
+        	callback=self.set_freq,
+        	label='freq',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._freq_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_freq_sizer,
+        	value=self.freq,
+        	callback=self.set_freq,
+        	minimum=88.0e6,
+        	maximum=107.9e6,
+        	num_steps=1000,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_freq_sizer)
+        self._enable_chooser = forms.radio_buttons(
+        	parent=self.GetWin(),
+        	value=self.enable,
+        	callback=self.set_enable,
+        	label='enable',
+        	choices=[0,1],
+        	labels=[],
+        	style=wx.RA_HORIZONTAL,
+        )
+        self.Add(self._enable_chooser)
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+        	",".join(("", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_sink_0.set_center_freq(freq, 0)
+        self.uhd_usrp_sink_0.set_normalized_gain(0.5, 0)
+        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=int(samp_rate * 1.0),
+                decimation=audio_rate * audio_interp,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.blocks_wavfile_source_1 = blocks.wavfile_source("/home/john/IRG/Music/John Prine - The Sins of Memphisto.wav", True)
+        self.blocks_wavfile_source_0 = blocks.wavfile_source("/home/john/IRG/Music/Inspired But Too Tired Acoustic.wav", True)
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_vff((25, ))
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((25, ))
+        self.blks2_selector_0 = grc_blks2.selector(
+        	item_size=gr.sizeof_gr_complex*1,
+        	num_inputs=2,
+        	num_outputs=1,
+        	input_index=enable,
+        	output_index=0,
+        )
+        self.analog_wfm_tx_0_0 = analog.wfm_tx(
+        	audio_rate=audio_rate,
+        	quad_rate=audio_rate * audio_interp,
+        	tau=75e-6,
+        	max_dev=5e3,
+        )
+        self.analog_wfm_tx_0 = analog.wfm_tx(
+        	audio_rate=audio_rate,
+        	quad_rate=audio_rate * audio_interp,
+        	tau=75e-6,
+        	max_dev=5e3,
+        )
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_wfm_tx_0, 0), (self.blks2_selector_0, 1))    
+        self.connect((self.analog_wfm_tx_0_0, 0), (self.blks2_selector_0, 0))    
+        self.connect((self.blks2_selector_0, 0), (self.rational_resampler_xxx_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.analog_wfm_tx_0_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.analog_wfm_tx_0, 0))    
+        self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_multiply_const_vxx_0, 0))    
+        self.connect((self.blocks_wavfile_source_1, 0), (self.blocks_multiply_const_vxx_0_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.uhd_usrp_sink_0, 0))    
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self._freq_slider.set_value(self.freq)
+        self._freq_text_box.set_value(self.freq)
+        self.uhd_usrp_sink_0.set_center_freq(self.freq, 0)
+
+    def get_enable(self):
+        return self.enable
+
+    def set_enable(self, enable):
+        self.enable = enable
+        self._enable_chooser.set_value(self.enable)
+        self.blks2_selector_0.set_input_index(int(self.enable))
+
+    def get_audio_rate(self):
+        return self.audio_rate
+
+    def set_audio_rate(self, audio_rate):
+        self.audio_rate = audio_rate
+
+    def get_audio_interp(self):
+        return self.audio_interp
+
+    def set_audio_interp(self, audio_interp):
+        self.audio_interp = audio_interp
+
+
+if __name__ == '__main__':
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = top_block()
+    tb.Start(True)
+    tb.Wait()
