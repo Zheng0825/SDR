9ea007e3ae6d8438d30d82c59c3f6dba8ec95fa1
John McCormack <mccormack.wgsi@gmail.com>
2016-02-06 20:44:05 -0500

    did a quick run through of how socket-io works with flask

--- SDR/Flowgraphs/testingsocketio/app.py
+++ SDR/Flowgraphs/testingsocketio/app.py
@@ -1,73 +1,138 @@
-"""
-Demo Flask application to test the operation of Flask with socket.io
-Aim is to create a webpage that is constantly updated with random numbers from a background python process.
-30th May 2014
-"""
+#!/usr/bin/env python
 
-# Start with a basic flask app webpage.
-from flask.ext.socketio import SocketIO, emit
-from flask import Flask, render_template, url_for, copy_current_request_context
-from random import random
-from time import sleep
-from threading import Thread, Event
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on available packages.
+async_mode = None
 
+if async_mode is None:
+    try:
+        import eventlet
+        async_mode = 'eventlet'
+    except ImportError:
+        pass
 
-__author__ = 'slynn'
+    if async_mode is None:
+        try:
+            from gevent import monkey
+            async_mode = 'gevent'
+        except ImportError:
+            pass
+
+    if async_mode is None:
+        async_mode = 'threading'
+
+    print('async_mode is ' + async_mode)
+
+# monkey patching is necessary because this application uses a background
+# thread
+if async_mode == 'eventlet':
+    import eventlet
+    eventlet.monkey_patch()
+elif async_mode == 'gevent':
+    from gevent import monkey
+    monkey.patch_all()
+
+import time
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = True
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
 
-#turn the flask app into a socketio app
-socketio = SocketIO(app)
 
-#random number Generator Thread
-thread = Thread()
-thread_stop_event = Event()
-
-class RandomThread(Thread):
-    def __init__(self):
-        self.delay = 1
-        super(RandomThread, self).__init__()
-
-    def randomNumberGenerator(self):
-        """
-        Generate a random number every 1 second and emit to a socketio instance (broadcast)
-        Ideally to be run in a separate thread?
-        """
-        #infinite loop of magical random numbers
-        print "Making random numbers"
-        while not thread_stop_event.isSet():
-            number = round(random()*10, 3)
-            print number
-            socketio.emit('newnumber', {'number': number}, namespace='/test')
-            sleep(self.delay)
-
-    def run(self):
-        self.randomNumberGenerator()
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        time.sleep(10)
+        count += 1
+        socketio.emit('my response',
+                      {'data': 'Server generated event', 'count': count},
+                      namespace='/test')
 
 
 @app.route('/')
 def index():
-    #only by sending this page first will the client be connected to the socketio instance
+    global thread
+    if thread is None:
+        thread = Thread(target=background_thread)
+        thread.daemon = True
+        thread.start()
     return render_template('index.html')
+
+
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socketio.on('join', namespace='/test')
+def join(message):
+    join_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': 'In rooms: ' + ', '.join(rooms()),
+          'count': session['receive_count']})
+
+
+@socketio.on('leave', namespace='/test')
+def leave(message):
+    leave_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': 'In rooms: ' + ', '.join(rooms()),
+          'count': session['receive_count']})
+
+
+@socketio.on('close room', namespace='/test')
+def close(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response', {'data': 'Room ' + message['room'] + ' is closing.',
+                         'count': session['receive_count']},
+         room=message['room'])
+    close_room(message['room'])
+
+
+@socketio.on('my room event', namespace='/test')
+def send_room_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': session['receive_count']},
+         room=message['room'])
+
+
+@socketio.on('disconnect request', namespace='/test')
+def disconnect_request():
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': 'Disconnected!', 'count': session['receive_count']})
+    disconnect()
+
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    # need visibility of the global thread object
-    global thread
-    print('Client connected')
+    emit('my response', {'data': 'Connected', 'count': 0})
 
-    #Start the random number generator thread only if the thread has not been started before.
-    if not thread.isAlive():
-        print "Starting Thread"
-        thread = RandomThread()
-        thread.start()
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
-    print('Client disconnected')
+    print('Client disconnected', request.sid)
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
