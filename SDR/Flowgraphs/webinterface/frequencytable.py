cd3fe9642b2805e8d59968b9b1786f8e1ef0ee12
John McCormack <mccormack.wgsi@gmail.com>
2016-02-11 14:03:34 -0500

    Finished up the first draft of the python object for handling the frequency table

--- SDR/Flowgraphs/webinterface/frequencytable.py
+++ SDR/Flowgraphs/webinterface/frequencytable.py
@@ -2,28 +2,35 @@
 class FrequencyTable:
 
     freqs = [900000000, 915000000, 920000000, 2200000000, 2400000000, 2500000000, 5725000000]
-    current_freq
-    current_index
+    current_freq = 0 
+    current_index = 0
 
     def __init__(self):
-        with self._lock:
-            current_index = 0
-            self.current_freq = freqs[current_index]
+        self.current_index = 0
+        self.current_freq = self.freqs[self.current_index]
         
 
     def increase_freq(self):
-        with self._lock:
-            if current_index < length(freqs):
-                current_index += 1
-                current_freq = freqs[current_index]
-            else:
-                pass
+        if self.current_index < len(self.freqs) - 1:
+            self.current_index += 1
+            self.current_freq = self.freqs[self.current_index]
+        else:
+            pass
 
 
     def decrease_freq(self):
-        with self._lock:
-            if current_index > 0:
-                current_index -= 1
-                current_freq = freqs[current_index]
-            else:
-                pass
+        if self.current_index > 0:
+            self.current_index -= 1
+            self.current_freq = self.freqs[self.current_index]
+        else:
+            pass
+
+if __name__ == "__main__":
+    ftable = FrequencyTable()
+
+    for i in range(10):
+        ftable.increase_freq()
+        print(ftable.current_freq)
+    for i in range(10):
+        ftable.decrease_freq()
+        print(ftable.current_freq)
