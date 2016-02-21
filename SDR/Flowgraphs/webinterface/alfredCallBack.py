ed03d56c09e0140a8201266d86bc4ad4d028e7ae
John McCormack <mccormack.wgsi@gmail.com>
2016-02-21 14:58:32 -0500

    updated the alfred call back again

--- SDR/Flowgraphs/webinterface/alfredCallBack.py
+++ SDR/Flowgraphs/webinterface/alfredCallBack.py
@@ -15,38 +15,45 @@ def send (lines):
 	data = []
 	
 	for line in lines:
-	#"{"Macaddr" : "freq\x01"}"
-		print (line)
+		if line == "\n":
+			pass
+		else:
+			print (line)
 
-		frame = line.strip()[2:-3].split(', ')
-		freqandtime = frame[1].split('\\')[0]
-		freq,time = freqandtime[1:].split('%')
-		time = time[:-2]
-		mac = frame[0]
+			frame = line.strip()[2:-3].split(', ')
+			freqandtime = frame[1].split('\\')[0]
+			freq,time = freqandtime[1:].split('%')
+			mac = frame[0]
 
-		if not data:
-			data = [mac,freq,time]
-		elif data[2] >= time:
-			data = [mac,freq,time]
+			if not data:
+				data = [mac,freq,time]
+			elif float(data[2]) <= float(time):
+				print("HERE")
+				data = [mac,freq,time]
 
-
+	print ("Outputting: " + str(data[1]))
 	socketIO = SocketIO('127.0.0.1', 5000)
 	socketIO.emit('alfred set freq', str(data[1]))
 	socketIO.wait(1)
 
-if __name__ == '__main__':
+#use this function for testing
+def test()
+	#Some dummy data if you need to do testing
+	#line = '{ "c2:4c:e0:9f:80:07", "918000000%1456077160.77\x0a" },'
+	lines = ['{ "6e:28:26:a3:71:dd", "915000000%1456083136.18\\x0a" },\n', '{ "f6:84:ae:73:38:0c", "920000000%1456083275.63\\x0a" },\n']
+	send(lines)
 
+
+if __name__ == '__main__':
 	lines = []
 
 	for line in sys.stdin:	
 		lines.append(line)
 	send(lines)
+	
+	#test()
 
-	'''
-	Some dummy data if you need to do testing
-	line = '{ "c2:4c:e0:9f:80:07", "918000000%1456077160.77\x0a" },'
-	lines = []
-	lines.append(line)
-	send(lines)
-	'''
+
+	
+
 
