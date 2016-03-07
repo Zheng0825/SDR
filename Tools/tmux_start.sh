d99b530078dea020e9d4f43643874dc24530c140
Bradley Trowbridge <bradley.trowbridge@yahoo.com>
2016-03-07 14:02:08 -0500

    Updating tmux_start.sh

--- Tools/tmux_start.sh
+++ Tools/tmux_start.sh
@@ -1,21 +1,44 @@
 #!/bin/sh
 
+# Requires sshpasss
+# sudo apt-get install sshpass
+
 SESSION=$USER
 
+# Function to connect to all sessins
+# format should be:
+#		connect user pass ip
+connect(){
+	tmux new-window
+	#tmux split-window -v
+
+	#tmux select-pane -t 0
+
+	ssh="sshpass -p $2 ssh -X $1@192.168.1.101"
+	tmux send-keys "$ssh" C-m
+	
+	tmux send-keys "cd SDR/SDR/Flowgraphs" C-m
+
+
+
+}
+
+user='vtclab'
+pass='vtclab'
+
+# Start Session
 tmux -2 new-session -d -s $SESSION
 
-tmux split-window -v
-tmux select-pane -t 0
-tmux send-keys "sshpass -p vtclab ssh  -X vtclab@192.168.1.101" C-m
-tmux send-keys "cd John/SDR/SDR/Flowgraphs/webinterface" C-m
-tmux select-pane -t 1
-tmux send-keys "sshpass -p vtclab ssh -X vtclab@192.168.1.101" C-m
-tmux send-keys "cd John/SDR/SDR/Flowgraphs/webinterface" C-m
-tmux new-window
-tmux split-window -v
-tmux select-pane -t 0
-tmux send-keys "cd ~/John/SDR/SDR/Flowgraphs/webinterface" C-m
-tmux select-pane -t 1
-tmux send-keys "cd ~/John/SDR/SDR/Flowgraphs/webinterface" C-m
+connect $user $pass
 
+# Connect to setup session
 tmux -2 attach-session -t $SESSION
+
+
+# IP List
+# 192.168.1.100
+# 192.168.1.101
+# 192.168.1.102
+# 192.168.1.106
+# 192.168.1.107
+# 192.168.1.109
