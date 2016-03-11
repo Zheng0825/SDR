e9de8e124d780fcc10f7b109c2cceef205a3b851
John McCormack <mccormack.wgsi@gmail.com>
2016-03-11 15:13:56 -0500

    Prepping for next test

--- TestResults/HopMess/nobatmantests.sh
+++ TestResults/HopMess/nobatmantests.sh
@@ -4,19 +4,23 @@ mkdir NoBatman
 
 cd NoBatman
 
+mkdir 905-915
+
+cd 905-915
+
 mkdir Standard
 
 cd Standard
 
-echo "No Batman TTL 128 Packet RX/TX 915/928 Gain 47 " > NoBatTTL128Test1000_1.txt && ping -c 1000 192.168.200.109 >> NoBat1000_1.txt
+echo "No Batman TTL 128 Packet RX/TX 905/915 Gain 47 " > NoBatTTL128Test1000_1.txt && ping -c 1000 192.168.200.109 >> NoBat1000_1.txt
 
 espeak "First Packet Test Complete"
 
-echo "No Batman TTL 128 Packet RX/TX 915/928 Gain 47 " > NoBatTTL128Test1000_2.txt && ping -c 1000 192.168.200.109 >> NoBat1000_2.txt
+echo "No Batman TTL 128 Packet RX/TX 905/915 Gain 47 " > NoBatTTL128Test1000_2.txt && ping -c 1000 192.168.200.109 >> NoBat1000_2.txt
 
 espeak "Second Packet Test Complete"
 
-echo "No Batman TTL 128 Packet RX/TX 915/928 Gain 47 " > NoBatTTL128Test1000_3.txt && ping -c 1000 192.168.200.109 >> NoBat1000_3.txt
+echo "No Batman TTL 128 Packet RX/TX 905/915 Gain 47 " > NoBatTTL128Test1000_3.txt && ping -c 1000 192.168.200.109 >> NoBat1000_3.txt
 
 espeak "Third Packet Test Complete"
 espeak "Changing Tests"
@@ -31,11 +35,11 @@ mkdir TTLChange
 
 cd TTLChange
 
-echo "No Batman TTL 128 Packet RX/TX 915/928 Gain 47 " > NoBatTTL128Test1000.txt && ping -c 1000 -t 128 192.168.200.109 >> NoBatTTL128Test1000.txt
+echo "No Batman TTL 128 Packet RX/TX 905/915 Gain 47 " > NoBatTTL128Test1000.txt && ping -c 1000 -t 128 192.168.200.109 >> NoBatTTL128Test1000.txt
 
 espeak "TTL 128 Complete"
 
-echo "No Batman TTL 255 Packet RX/TX 915/928 Gain 47 " > NoBatTTL255SizeTest1000.txt && ping -c 1000 -t 255 192.168.200.109 >> NoBatTTL255Test1000.txt
+echo "No Batman TTL 255 Packet RX/TX 905/915 Gain 47 " > NoBatTTL255SizeTest1000.txt && ping -c 1000 -t 255 192.168.200.109 >> NoBatTTL255Test1000.txt
 
 espeak "TTYL 255 Packet Complete"
 
@@ -50,11 +54,11 @@ mkdir PacketSizeChange
 
 cd PacketSizeChange
 
-echo "No Batman 32byte Packet RX/TX 915/928 Gain 47 " > NoBat32bytePacketSizeTest1000.txt && ping -c 1000 -s 24 192.168.200.109 >> NoBat32bytePacketSizeTest1000.txt
+echo "No Batman 32byte Packet RX/TX 905/915 Gain 47 " > NoBat32bytePacketSizeTest1000.txt && ping -c 1000 -s 24 192.168.200.109 >> NoBat32bytePacketSizeTest1000.txt
 
 espeak "32 Byte Packet Test Complete"
 
-echo "No Batman 16byte Packet RX/TX 915/928 Gain 47 " > NoBat16bytePacketSizeTest1000.txt && ping -c 1000 -s 16 192.168.200.109 >> NoBat16bytePacketSizeTest1000.txt
+echo "No Batman 16byte Packet RX/TX 905/915Gain 47 " > NoBat16bytePacketSizeTest1000.txt && ping -c 1000 -s 16 192.168.200.109 >> NoBat16bytePacketSizeTest1000.txt
 
 espeak " 16 bye packet complete" 
 
