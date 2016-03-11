ca34a04d52c9e759399ebef1b41b06893769c392
John McCormack <mccormack.wgsi@gmail.com>
2016-03-11 13:00:00 -0500

    Added a test script for testing the nonbatman version

--- SDR/WebInterface/alfredCallBack.py
+++ SDR/WebInterface/alfredCallBack.py
@@ -42,13 +42,14 @@ def send (lines):
 def test():
 	#Some dummy data if you need to do testing
 	#line = '{ "c2:4c:e0:9f:80:07", "918000000%1456077160.77\x0a" },'
-	lines = ['{ "6e:28:26:a3:71:dd", "915000000%1456083136.18\\x0a" },\n', '{ "f6:84:ae:73:38:0c", "920000000%1456083275.63\\x0a" },\n']
+	lines = ['{ "6e:28:26:a3:71:dd", "915000000%1456083136.18\\x0a" },\n', '{ "f6:84:ae:73:38:0c", "922000000%1459083275.63\\x0a" },\n']
 	send(lines)
 
 
 if __name__ == '__main__':
 	lines = []
 
+	
 	for line in sys.stdin:	
 		lines.append(line)
 	
