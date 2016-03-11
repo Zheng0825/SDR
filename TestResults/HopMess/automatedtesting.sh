20df6aca35c4f8faab25036d002666700942da41
John McCormack <mccormack.wgsi@gmail.com>
2016-03-11 13:02:00 -0500

    missed some files

--- TestResults/HopMess/automatedtesting.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#echo "Four Hops Alternating RX/TX 915/928 AVG Gain 47 " > 4HopAltFreqTest.txt && ping -c 1000 192.168.200.109 >> 4HopAltFreqTest.txt
-
-echo "Three Hops Alternating RX/TX 915/928 AVG Gain 47 " > 3HopAltFreqTest.txt && ping -c 5 192.168.200.102 >> 3HopAltFreqTest.txt
-
-echo "Two Hops Alternating RX/TX 915/928 AVG Gain 47 " > 2HopAltFreqTest.txt && ping -c 5 192.168.200.101 >> 2HopAltFreqTest.txt
-
-echo "One Hops Alternating RX/TX 915/928 AVG Gain 47 " > 1HopAltFreqTest.txt && ping -c 5 192.168.200.106 >> 1HopAltFreqTest.txt
---
+++ TestResults/HopMess/nobatmantests.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+mkdir NoBatman
+
+cd NoBatman
+
+mkdir Standard
+
+echo "No Batman TTL 128 Packet RX/TX 915/928 Gain 47 " > NoBatTTL128Test1000_1.txt && ping -c 1000 -t 128 192.168.200.109 >> NoBatTTL128Test1000_1.txt
+
+espeak "First Packet Test Complete"
+
+echo "No Batman TTL 128 Packet RX/TX 915/928 Gain 47 " > NoBatTTL128Test1000_2.txt && ping -c 1000 -t 128 192.168.200.109 >> NoBatTTL128Test1000_2.txt
+
+espeak "Second Packet Test Complete"
+
+echo "No Batman TTL 128 Packet RX/TX 915/928 Gain 47 " > NoBatTTL128Test1000_3.txt && ping -c 1000 -t 128 192.168.200.109 >> NoBatTTL128Test1000_3.txt
+
+espeak "Third Packet Test Complete"
+espeak "Changing Tests"
+
+
+
+cd Standard
+
+cd ..
+
+mkdir TTLChange
+
+echo "No Batman TTL 128 Packet RX/TX 915/928 Gain 47 " > NoBatTTL128Test1000.txt && ping -c 1000 -t 128 192.168.200.109 >> NoBatTTL128Test1000.txt
+
+espeak "TTL 128 Complete"
+
+echo "No Batman TTL 255 Packet RX/TX 915/928 Gain 47 " > NoBatTTL255SizeTest1000.txt && ping -c 1000 -t 255 192.168.200.109 >> NoBatTTL255Test1000.txt
+
+espeak "TTYL 255 Packet Complete"
+
+espeak "Changing Tests"
+
+
+cd TTLChange
+
+cd ..
+
+mkdir PacketSizeChange
+
+cd PacketSizeChange
+
+echo "No Batman 32byte Packet RX/TX 915/928 Gain 47 " > NoBat32bytePacketSizeTest1000.txt && ping -c 1000 -s 24 192.168.200.109 >> NoBat32bytePacketSizeTest1000.txt
+
+espeak "32 Byte Packet Test Complete"
+
+echo "No Batman 16byte Packet RX/TX 915/928 Gain 47 " > NoBat16bytePacketSizeTest1000.txt && ping -c 1000 -s 16 192.168.200.109 >> NoBat16bytePacketSizeTest1000.txt
+
+espeak " 16 bye packet complete" 
+
+espeak "All Current tests complete, please change frequency and run again"
+
+cd ..
